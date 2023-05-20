aba04ccd0bd82fd73550376ad4de33da757b902d
Jason Chionh <jasonchionh@gmail.com>
2023-05-20 02:16:05 -0700

    Adding word dictionary behavior

---
+++ src/WordDict.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <unordered_set>
+#include <string>
+
+class WordDict
+{
+    public:
+    static const std::unordered_set<std::string> SOWPODS_3_5;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 #include <aws/lambda-runtime/runtime.h>
+
+#include "WordDict.h"
+#include "json.hpp"
+
+using namespace aws::lambda_runtime;
+
+invocation_response JzapWordQueryHandler(invocation_request const& request)
+{
+    // extract our query word
+    auto payload = json::JSON::Load(request.payload);
+    std::string wordQuery = payload["word"].ToString();
+
+    // change to uppercase
+    std::transform(wordQuery.begin(), wordQuery.end(), wordQuery.begin(), ::tolower);
+
+    // query to find the word in our dict
+    const bool wordFound = !(WordDict::SOWPODS_3_5.find(wordQuery) == WordDict::SOWPODS_3_5.end());
+    const std::string&& queryResult = wordFound ? "true" : "false";
+
+    std::cout << "Word: " << wordQuery << ", found? " << queryResult << std::endl;
+
+    std::string resultString("{ \"result\": ");
+    resultString += queryResult;
+    resultString += " }";
+
+    return invocation_response::success(resultString, "application/json");
+}
 
 int main()
 {
-    std::cout << "Hello Console" << std::endl;
+    run_handler(JzapWordQueryHandler);
     return 0;
 }
